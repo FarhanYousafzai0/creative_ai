87c277f61e605df9cef7e8e87ccb6aeb31998f22
Farhan Yousafzai <coderwithferry@gmail.com>
2025-12-16 18:50:33 +0500

    Ai_Landing Page

---
+++ app/lib/Data.js
@@ -0,0 +1,8 @@
+export const navLinks = [
+    { name: "Home", href: "#home", active: true },
+    { name: "About", href: "#about" },
+    { name: "Portfolio", href: "#portfolio" },
+    { name: "Contact", href: "#contact" },
+    { name: "FAQ", href: "#faq" },
+  ];
+  
